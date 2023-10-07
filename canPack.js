f1b5a6efc745f7f73693a4464d628b3eb5d02fea
Mina Nagy <mnagy@icinformatics.com>
2023-10-07 20:05:36 +0300

    add negative cases

--- canPack.js
+++ canPack.js
@@ -1,5 +1,14 @@
 function canPack(bigCount, smallCount, goal) {
-    return false;
+
+    // If any of the parameters are negative, return false.
+    if (bigCount < 0 || smallCount < 0 || goal < 0)
+        return false;
+
+    // the sum of the kilos of bigCount and smallCount must be at least equal to the value of goal
+    if (bigCount*5 + smallCount * 1 < goal)
+        return false;
+
+    return true;
 }
 
 function execute(bigCount, smallCount, goal){
@@ -9,6 +18,13 @@ function execute(bigCount, smallCount, goal){
         "goal:", goal,
         "result:", canPack(bigCount, smallCount, goal));
 }
+
+// test cases
+execute(2, -1, 9);
+execute(-2, 0, 9);
+execute(2, 0, -9);
+
+execute(1, 0, 9);
 
 execute(2, 0, 9);
 execute(1, 5, 9);
