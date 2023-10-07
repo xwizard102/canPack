41448bfaab4e3dc28a39ea509dedc0d9c51d7e46
Mina Nagy <mnagy@icinformatics.com>
2023-10-07 21:12:42 +0300

    add solution

--- canPack.js
+++ canPack.js
@@ -1,12 +1,28 @@
 function canPack(bigCount, smallCount, goal) {
 
+    let kilosPerBig = 5;
+
     // If any of the parameters are negative, return false.
     if (bigCount < 0 || smallCount < 0 || goal < 0)
         return false;
 
     // the sum of the kilos of bigCount and smallCount must be at least equal to the value of goal
-    if (bigCount*5 + smallCount * 1 < goal)
+    if (bigCount * kilosPerBig + smallCount < goal)
         return false;
+
+    // when remainder is NOT covered by small count
+    var rem = goal % kilosPerBig;
+    if (rem > 0 && rem > smallCount) return false;
+
+    // otherwise remainder is covered
+    // and quotient is covered by big count
+    var quot = Math.floor(goal / kilosPerBig);
+    if (quot <= bigCount) return true;
+    
+    // otherwise quotient is NOT covered by big count
+    // and remaining kilos is NOT covered by small count
+    var kilosToGoal = quot - (bigCount*kilosPerBig);
+    if (kilosToGoal > smallCount) return false;
 
     return true;
 }
@@ -20,11 +36,15 @@ function execute(bigCount, smallCount, goal){
 }
 
 // test cases
-execute(2, -1, 9);
-execute(-2, 0, 9);
-execute(2, 0, -9);
 
-execute(1, 0, 9);
+console.log("\nNegative Numbers:\n");
+execute(2, -1, 9); // false
+execute(-2, 0, 9); // false
+execute(2, 0, -9); // false
 
-execute(2, 0, 9);
-execute(1, 5, 9);
+console.log("\nSum less than goal:\n");
+execute(1, 0, 9); // false
+
+console.log("\nProvided Test Cases:\n");
+execute(2, 0, 9); // false
+execute(1, 5, 9); // true
